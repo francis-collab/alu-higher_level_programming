d68c11c3690fb40ff98c61be3c7c25c594a9858c
francis-collab <f.mutabazi1@alustudent.com>
2024-11-07 01:06:21 +0200

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 1-square
+Defines a Square class with size attribute.
+"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the square with a size attribute.
+        
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
