40c5a35e32d2bc02f487381f782610a59bdb2511
francis-collab <f.mutabazi1@alustudent.com>
2024-11-07 01:18:09 +0200

    Add 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,12 +3,13 @@
 Defines a Square class with size attribute and validation.
 """
 
+
 class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
         """Initializes the square with a size attribute.
-        
+
         Args:
             size (int): The size of the square. Default is 0.
         """
