e98a3ba5ddfc00b8d15d044b497e1d14eb1840d9
francis-collab <f.mutabazi1@alustudent.com>
2024-11-07 07:21:44 +0200

    Add MyList class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for MyList class."""
+
+
+class MyList(list):
+    """Inherits from list."""
+
+    def print_sorted(self):
+        """Prints the list in ascending order."""
+        print(sorted(self))
