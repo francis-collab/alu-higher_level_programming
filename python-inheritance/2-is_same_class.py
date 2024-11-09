f2a55f50bb6c02a7fb97cb77b487167f5d7a3120
francis-collab <f.mutabazi1@alustudent.com>
2024-11-09 14:15:38 +0200

    Add is_same_class function

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module for is_same_class function."""
+
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class, False otherwise."""
+    return type(obj) == a_class
