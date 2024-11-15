fc423b4b39aff3fdac800d8a52d9566a90ba5157
francis-collab <f.mutabazi1@alustudent.com>
2024-11-16 00:03:45 +0200

    Full description of the table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Task: Print the full description of the table first_table from the database hbtn_0c_0
+-- This script will show the creation statement of first_table
+
+SHOW CREATE TABLE first_table;
