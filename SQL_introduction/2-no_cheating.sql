d621dbb267b54851d2270c529e3c9492030db087
francis-collab <f.mutabazi1@alustudent.com>
2024-11-16 00:18:00 +0200

    script that updates the score of Bob to 10 in the table second_table

---
+++ SQL_introduction/2-no_cheating.sql
@@ -0,0 +1,8 @@
+
+-- Task: Update the score of Bob to 10 in the table second_table without using Bob’s id
+-- This script will update Bob's score based on the name field
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
