c0cc1f45b6b9ecf96b68184e14b6d20d7e39c5cc
francis-collab <f.mutabazi1@alustudent.com>
2024-11-27 14:24:41 +0200

    Add task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Fetches the value of X-Request-Id from a URL using requests and sys"""
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header of the response.
+"""
 import sys
-import requests
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    
+    # Create a request object
+    request = urllib.request.Request(url)
+    
+    try:
+        with urllib.request.urlopen(request) as response:
+            headers = response.headers
+            x_request_id = headers.get("X-Request-Id")
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("X-Request-Id header is not present")
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
