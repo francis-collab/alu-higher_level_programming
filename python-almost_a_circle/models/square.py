1359ea988ebd06680fe2eff8430fa989fd9bf941
francis-collab <f.mutabazi1@alustudent.com>
2024-11-28 19:33:27 +0200

    And now, the Square!

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Square module """
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of the square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
