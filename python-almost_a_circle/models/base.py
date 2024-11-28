727f8159c456507bbfbc6c54ee2614fd95ba648b
francis-collab <f.mutabazi1@alustudent.com>
2024-11-28 20:17:07 +0200

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-""" Base module """
+"""Base module."""
+import json
 
 
 class Base:
-    """Base class for managing id attribute in future classes"""
+    """Base class for managing id attribute in future classes."""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
