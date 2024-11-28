e3bc17379e152a8e5122819b57486e37413a888d
francis-collab <f.mutabazi1@alustudent.com>
2024-11-28 19:49:14 +0200

    Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-""" Square module """
+"""Square module."""
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class inheriting from Rectangle"""
+    """Square class inheriting from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square, affecting width and height."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        """Return a string representation of the square"""
+        """Return a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
