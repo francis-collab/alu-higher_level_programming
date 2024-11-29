b116cb74ea2f55f26b89b78d2066a68e3db6f93b
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-29 18:07:11 +0200

    hbtn_header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """A script that:
 - takes in a URL,
-- sends a request to the URL and displays all headers
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
 import urllib.request
@@ -9,22 +10,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    # Define headers
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "*/*",
-    }
-
-    # Create a request object
-    request = urllib.request.Request(url, headers=headers)
-    
-    try:
-        with urllib.request.urlopen(request) as response:
-            # Print all headers
-            for header, value in response.headers.items():
-                print(f"{header}: {value}")
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print("URL Error: {}".format(e.reason))
-
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
