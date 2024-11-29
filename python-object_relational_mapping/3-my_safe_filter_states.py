08a44bb3bf0cd0bdd19929b83df0589e989e8bb1
francis-collab <f.mutabazi1@alustudent.com>
2024-11-29 16:28:10 +0200

    SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa where name matches the argument, safe from SQL injections
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the query, using parameterized queries to avoid SQL injection
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    # Print the rows
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
