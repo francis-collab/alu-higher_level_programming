3953ff0950e87ea73c9838c0f96c7a92317ea4a8
francis-collab <f.mutabazi1@alustudent.com>
2025-02-06 07:34:58 -0800

    Add 6-completed_tasks.js

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
+    for (const task of tasks) {
+      if (task.completed) {
+        if (completedTasks[task.userId]) {
+          completedTasks[task.userId]++;
+        } else {
+          completedTasks[task.userId] = 1;
+        }
+      }
+    }
+    console.log(completedTasks);
+  }
+});
