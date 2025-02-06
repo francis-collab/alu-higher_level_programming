6af3333a43969fbb48ae33d70c244d7f4c264072
francis-collab <f.mutabazi1@alustudent.com>
2025-02-06 07:46:40 -0800

    Fix 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,12 +6,19 @@ const url = process.argv[2];
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
+    const wedgeId = '18';
     let count = 0;
+
     for (const film of films) {
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
+      for (const character of film.characters) {
+        if (character.includes(`/api/people/${wedgeId}/`)) {
+          count++;
+          break;
+        }
       }
     }
     console.log(count);
+  } else {
+    console.log(error);
   }
 });
